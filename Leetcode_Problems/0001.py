936a40aabd6e027ebb94becefdbdbd32845d13c3
bryce <76966016+brycegallo@users.noreply.github.com>
2025-05-09 17:25:44 -0600

    Solve 0001 - Two Sum in Python

---
+++ Leetcode_Problems/0001.py
@@ -0,0 +1,20 @@
+# Leetcode 0001 - Two Sum
+# Easy - Arrays & Hashing
+# Given an array of integers num and an integer taraget, return the indices of the two numbers such that they add up to target
+# You may assume that each input would have exactly one solution, and you may not use the same element twice
+# You can return the answer in any order
+
+# Hash Map Solution
+# Complexities:
+# Time : O(n)
+# Space: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # hashmap[difference] = index
+        hashmap = {}
+
+        for index, number in enumerate(nums):
+            difference = target - number
+            if difference in hashmap:
+                return [hashmap[difference], index]
+            hashmap[number] = index
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ LeetCode Problems, Data Structure Implementations, and other tools for studying 
 ## LeetCode Problems
 C:      18
 Go:     4
-Python: 2
+Python: 4
 
 Total:  24
