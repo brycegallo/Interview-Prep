e596269887080dbe96171b532d527ff3e78e74c9
bryce <76966016+brycegallo@users.noreply.github.com>
2025-05-09 16:34:49 -0600

    Solve 0242 - Valid Anagram in Python

---
+++ Leetcode_Problems/0242.py
@@ -0,0 +1,23 @@
+# Leetcode 0242: Valid Anagram
+# Easy - Arrays and Hashing
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise
+# s is guaranteed to be at least 1 character, t is not
+
+# Hash Map Solution
+# Complexities:
+# Time : O(m + n)
+# Space: O(1)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if strings are not the same length, they can't be anagrams
+        if len(s) != len(t):
+            return False
+        
+        # create empty hash maps ("dictionaries" in python)
+        s_map, t_map = {}, {}
+
+        for i in range(len(s)):
+            # set key and value = 1 + current value (will be 0 if not present)
+            s_map[s[i]] = 1 + s_map.get(s[i], 0)
+            t_map[t[i]] = 1 + t_map.get(t[i], 0)
+        return s_map == t_map
