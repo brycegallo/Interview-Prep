b931f48f6f865e8a74606ca4269a1f86b4e048ca
bryce <76966016+brycegallo@users.noreply.github.com>
2025-06-05 14:42:14 -0600

    Solve 0042 - Trapping Rain Water in C also update README.md

---
+++ Leetcode_Problems/0042.c
@@ -0,0 +1,25 @@
+// LeetCode 0042 - Trapping Rain Water
+// Hard - Two Pointers
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+// Single Pass, Two-Pointer Solution
+// Complexities:
+// Time : O(n)
+// Space: O(1)
+int trap(int* height, int heightSize) {
+    int result = 0;
+    int left = 0;
+    int right = heightSize - 1;
+    int left_max = height[left];
+    int right_max = height[right];
+
+    while (heightSize > 0 && left < right) {
+        if (left_max < right_max) {
+            left_max = left_max > height[++left] ? left_max : height[left];
+        } else {
+            right_max = right_max > height[--right] ? right_max : height[right];
+        }
+        result += left_max < right_max ? (left_max - height[left]) : (right_max - height[right]);
+    }
+    return result;
+}
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 LeetCode Problems, Data Structure Implementations, and other tools for studying algorithms
 
 ## LeetCode Problems
-C:      35<br/>
+C:      36<br/>
 Go:     4<br/>
 Python: 32<br/>
 
-Total:  71
+Total:  72
