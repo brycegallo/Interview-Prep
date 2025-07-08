2b6adf8bd74eb069919fe483426492ab2b23da58
bryce <76966016+brycegallo@users.noreply.github.com>
2025-07-07 19:30:25 -0600

    Solve 0287 - Find the Duplicate Number in Python also update README.md

---
+++ Leetcode_Problems/0287.py
@@ -0,0 +1,21 @@
+# LeetCode 0287 - Find the Duplicate Number
+# Medium - Linked List
+# Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+#
+# There is only one repeated number in nums, return this repeated number.
+#
+# You must solve the problem without modifying the array nums and using only constant extra space.
+
+# Floyd's Algorith Solution (Tortoise and Hare)
+# Complexities:
+# Time : O(n)
+# Space: O(1)
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow_1, slow_2, fast = nums[0], 0, nums[nums[0]]
+
+        while slow_1 != fast:
+            slow_1, fast = nums[slow_1], nums[nums[fast]]
+        while slow_1 != slow_2:
+            slow_1, slow_2 = nums[slow_1], nums[slow_2]
+        return slow_1
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ LeetCode Problems, Data Structure Implementations, and other tools for studying 
 ## LeetCode Problems
 C:      40<br/>
 Go:     4<br/>
-Python: 51<br/>
+Python: 52<br/>
 
-Total:  95
+Total:  96
