983608292185c7d38224d2c91e309274bebb82a7
bryce <76966016+brycegallo@users.noreply.github.com>
2025-08-06 11:29:44 -0600

    Solve 0703 - Kth Largest Element in a Stream in Python also update README.md

---
+++ Leetcode_Problems/0703.py
@@ -0,0 +1,29 @@
+# LeetCode 0703 - Kth Largest Element in a Stream
+# Easy - Heap / Priority Queue
+# You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+# You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+# 
+# Implement the KthLargest class:
+#    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+#    int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+# Min-Heap Solution
+# Complexities:
+# Time : O(m * log k) - m = number of calls made to add()
+# Space: O(k) - k = the input variable k
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
