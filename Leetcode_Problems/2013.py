555855aeb472952bde55f42f45a71cc9c1470a63
bryce <76966016+brycegallo@users.noreply.github.com>
2025-10-31 10:43:16 -0600

    Solve 2013 - Detect Squares in Python also update README.md

---
+++ Leetcode_Problems/2013.py
@@ -0,0 +1,37 @@
+# LeetCode 2013 - Detect Squares
+# Medium - Math & Geometry
+# You are given a stream of points on the X-Y plane. Design an algorithm that:
+#     Adds new points from the stream into a data structure. Duplicate points are allowed and should be treated as different points.
+#     Given a query point, counts the number of ways to choose three points from the data structure such that the three points and the query point form an axis-aligned square with positive area.
+# An axis-aligned square is a square whose edges are all the same length and are either parallel or perpendicular to the x-axis and y-axis.
+
+# Implement the DetectSquares class:
+#     DetectSquares() Initializes the object with an empty data structure.
+#     void add(int[] point) Adds a new point point = [x, y] to the data structure.
+#     int count(int[] point) Counts the number of ways to form axis-aligned squares with point point = [x, y] as described above.
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
+
+# Solution
+# Complexities:
+# Time : O(n) - O(n) for count(), O(1) for add()
+# Space: O(n)
+class DetectSquares:
+    def __init__(self):
+        self.points_count = defaultdict(int)
+        self.points = []
+
+    def add(self, point: List[int]) -> None:
+        self.points_count[tuple(point)] += 1
+        self.points.append(point)
+
+    def count(self, point: List[int]) -> int:
+        result = 0
+        px, py = point
+        for x, y in self.points:
+            if not (abs(py - y) != abs(px - x) or x == px or y == py):
+                result += self.points_count[(x, py)] * self.points_count[(px, y)]
+        return result
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ LeetCode Problems, Data Structure Implementations, and other tools for studying 
 ## LeetCode Problems
 C:      49<br/>
 Go:     4<br/>
-Python: 158<br/>
+Python: 159<br/>
 
-Total:  211
+Total:  212
