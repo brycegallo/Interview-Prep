3d8256ce50bc9f658f300511a3258af234ad35a3
bryce <76966016+brycegallo@users.noreply.github.com>
2025-11-24 13:27:45 -0600

    Solve 0543 - Diameter of Binary Tree in Go also update README.md

---
+++ Leetcode_Problems/0543.go
@@ -0,0 +1,35 @@
+// LeetCode 0543 - Diameter of Binary Tree
+// Easy - Trees
+// Given the root of a binary tree, return the length of the diameter of the tree.
+// The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+// The length of a path between two nodes is represented by the number of edges between them.
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// Iterative Depth-First Search Solution
+// Complexities:
+// Time : O(n)
+// Space: O(n) - O(log(n)) if tree is balanced, O(n) if degenerate
+func diameterOfBinaryTree(root *TreeNode) int {
+    result := 0
+    var dfs func(*TreeNode) int
+    dfs = func(root *TreeNode) int {
+        if root == nil {
+            return 0
+        }
+        left := dfs(root.Left)
+        right := dfs(root.Right)
+        result = max(result, left + right)
+
+        return 1 + max(left, right)
+    }
+    dfs(root)
+    return result
+}
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ LeetCode Problems, Data Structure Implementations, and other tools for studying 
 
 ## LeetCode Problems
 C:      52<br/>
-Go:     18<br/>
+Go:     19<br/>
 Python: 165<br/>
 
-Total:  235
+Total:  236
